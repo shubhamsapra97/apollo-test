82a84242e52348f60fc8c1af220c48533d1ec080
Shubham Sapra <shubham.sapra97@gmail.com>
2021-03-14 20:06:02 +0530

    Update schema.js

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -10,33 +10,10 @@ const typeDefs = gql`
         name: Name
         copies: Int
     }
-    type Status {
-        message: String!
-    }
-    type UserField {
-        _id: String
-        message: String!
-    }
     type Query {
         getLibraryBooks: [Books]
     }
-    type Mutation {
-        addUser(
-            email: String!
-        ): UserField
-        addBook(
-            name: String!
-            copies: Int
-        ): Status
-        borrowBook(
-            id: String!
-            userId: String!
-        ): Status
-        returnBook(
-            id: String!
-            userId: String!
-        ): Status
-    }
+    type Mutation {}
 `;
 
 module.exports = typeDefs;
